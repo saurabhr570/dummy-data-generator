cd2cab8d2de269c5fe89eb97671ed58dd9c4cd30
saurabhr570 <saurabhr570@gmail.com>
2023-01-13 15:42:08 +0530

    done

---
+++ javascript1/assignments/object/assign1.js
@@ -0,0 +1,8 @@
+//how to access all items from array usinf for loop in javascipt
+let numbers=[1,2,5,78,"work",true,0]
+for(number of numbers){
+    console.log(number)
+}
+for(let i=1;i<numbers.length;i++){
+    console.log(numbers[i])
+}
