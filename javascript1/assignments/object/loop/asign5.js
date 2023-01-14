34307229a2d1e6aff244f49318c358343a88cf51
saurabhr570 <saurabhr570@gmail.com>
2023-01-14 18:18:11 +0530

    okay

---
+++ javascript1/assignments/object/loop/asign5.js
@@ -0,0 +1,25 @@
+//write a program to read 5 numbers and print only even numbers 
+a=23
+b=25
+c=78
+d=76
+e=50
+if(a%2==0 && b%2==0 && c%2==0 && d%2==0 && e%2==0){
+      console.log(a)
+      console.log(b)
+      console.log(c)
+      console.log(d)
+      console.log(e)
+} 
+else if(b%2==0){
+    console.log(b)
+}
+else if(c%2==0){
+    console.log(c)
+}
+else if(d%2==0){
+    console.log(d)
+}
+else if(e%2==0){
+    console.log(e)
+}
---
+++ javascript1/practiceset.js
@@ -0,0 +1,27 @@
+//convert number in string
+/*a=100
+b=a.toString()
+console.log(typeof c)*/
+c=123
+
+/*function converttToString(num) {
+    return b=num.toString();
+}
+converttToString(134)
+console.log(typeof b)*/
+/*converttostring(123)
+converttostring();
+console.log(typeof c)
+*/
+//convert a string into array
+/*let arr="i am the best";
+let b=[...arr]
+console.log(b)*/
+/*let arr= "123"
+c=arr.split().reverse().toString().Math.floor()
+console.log(c)*/
+//convert string into number
+a='123'
+b=Math.floor(a)
+console.log(b)
+console.log(typeof b)
