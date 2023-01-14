dad17492fa176ce47afd960b2f74d11245f20aba
saurabhr570 <saurabhr570@gmail.com>
2023-01-14 18:18:26 +0530

    okay

---
+++ javascript1/array methods/method.js
@@ -0,0 +1,10 @@
+//to convert array into string
+/*let arr=[1,2,4,6,"rak","lakh"]
+a=arr.toString()
+console.log(a)
+console.log(typeof a)*/
+//to join the array
+/*let group=["sji",'jf','fjjk','hc','jjk']
+f=group.join('')
+console.log(f)*/
+//
