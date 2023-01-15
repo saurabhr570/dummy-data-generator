12d9f31659dcb1df85dac65572ff91e92ba21f14
saurabhr570 <saurabhr570@gmail.com>
2023-01-15 17:09:56 +0530

    okay

---
+++ javascript1/loopadd.js
@@ -0,0 +1,17 @@
+//how to sum values of an array
+let arr=[[1,2,3,4,7],[4,67,89,7,6,5,4]]
+let a=arr[0]
+let b=arr[1]
+let sum=0
+for(let i=0;i<a.length;i++){
+    sum+=a[i]
+}
+console.log(sum)
+/*let numbers = [1, 2, 3, 4, 5];
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+  sum += numbers[i];
+}
+console.log(sum); // 15*/
+
+
