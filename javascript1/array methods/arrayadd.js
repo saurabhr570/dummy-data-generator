690b1d7d403489daa28802d5e1e83d301a8505b7
saurabhr570 <saurabhr570@gmail.com>
2023-01-15 15:26:07 +0530

    done

---
+++ javascript1/array methods/arrayadd.js
@@ -0,0 +1,14 @@
+let arr=[[1,2,3,4,5],[4,67,89,7,6,5,4]]
+let a=arr[0]
+let b=arr[1]
+let d=a.concat(b)
+console.log(a)
+console.log(b)
+console.log(d)
+let add=a.join('')
+let add1=b.join('')
+console.log(add)
+console.log(add1)
+let num=Math.floor(add)
+let num1=Math.floor(add1)
+console.log(num+num1)
---
+++ javascript1/array methods/concait.js
@@ -0,0 +1,7 @@
+let arr=[[1,2,3,4,5],[4,67,89,7,6,5,4]]
+let a=arr[0]
+let b=arr[1]
+let d=a.concat(b)
+console.log(a)
+console.log(b)
+console.log(d)
