1d3f54560eb6ae986d4ba54d31ab8819973de00c
saurabhr570 <saurabhr570@gmail.com>
2023-01-18 14:55:49 +0530

    done

---
+++ javascript1/array methods/practiceMRF.js
@@ -0,0 +1,17 @@
+//create an araay of numbers and take input from user to add numbers to add this array
+/*let arr=[30,56,67,89,89,78]
+let a=55
+arr.push(a)
+console.log(arr)*/
+//create an araay of numbers and take input only if the given number is even from user to add numbers to add this 
+let arr1=[30,56,67,89,89,78]
+let a2=46
+ if(a2%2==0){
+    arr1.push(a2)
+    console.log("the given value"+a2+"is even")
+ }
+ else{
+    console.log("the given value"+a2+"is odd value")
+ }
+ console.log(arr1)
+ 
