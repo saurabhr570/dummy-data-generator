b461fde467f73597cfc99f5ca1ce3670ed9cd804
saurabhr570 <saurabhr570@gmail.com>
2023-02-02 14:32:49 +0530

    done

--- javascript1/oops/constructor.js
+++ javascript1/oops/constructor.js
@@ -1,16 +1,17 @@
-class college_id{
-    constructor(name,qualification,percent,pocketmoney){
-        this.name=name
-        this.qual=qualification
-        this.perc=percent
-        this.pm=pocketmoney
-    }
-    
+/*class college_id {
+  constructor(name, qualification, percent, pocketmoney) {
+    this.name = name;
+    this.qual = qualification;
+    this.perc = percent;
+    this.pm = pocketmoney;
+  }
+  school(a){
+    this.name=this.name+a
+
+  }
 }
-let chodry=new college_id("chodry","b.tech",80,5000)
-let saurabh=new college_id("saurabh","b.com",60,20000)
-let manish=new college_id("manish","bsc",61,10000)
-console.log(chodry)
-console.log(saurabh)
-console.log(manish)
-console.log(chodry.qual)
+let saurabh = new college_id()
+let manish = new college_id()
+saurabh.school(30000)
+manish.school(5000)
+console.log(saurabh.name)*/
---
+++ javascript1/oops/inheritense.js
@@ -0,0 +1,13 @@
+class java{
+    a="saurabh"
+    b="love"
+    c="you"
+}
+
+class python extends java{
+    d="puru"
+    e="love"
+    f="saurabh"
+}
+let a1=new python()
+console.log(a1)
