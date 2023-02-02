616f9e7a95473438eb9a39d16b2c08116a9cc6c9
saurabhr570 <saurabhr570@gmail.com>
2023-02-02 13:42:12 +0530

    done

---
+++ javascript1/oops/constructor.js
@@ -0,0 +1,16 @@
+class college_id{
+    constructor(name,qualification,percent,pocketmoney){
+        this.name=name
+        this.qual=qualification
+        this.perc=percent
+        this.pm=pocketmoney
+    }
+    
+}
+let chodry=new college_id("chodry","b.tech",80,5000)
+let saurabh=new college_id("saurabh","b.com",60,20000)
+let manish=new college_id("manish","bsc",61,10000)
+console.log(chodry)
+console.log(saurabh)
+console.log(manish)
+console.log(chodry.qual)
