763dbb03ce5eafc4de781272bb0d98e396ecca01
saurabhr570 <saurabhr570@gmail.com>
2023-04-20 19:27:49 +0530

    oay

---
+++ javascript1/codewithharry/four.js
@@ -0,0 +1,9 @@
+let a =document.getElementsByTagName("div")[0]
+let div=document.createElement("h1")
+div.innerHTML="<h2>hello world</h2>"
+//a.appendChild(div)
+a.append(div)
+//a.appendprepend(div)
+//a.before(div)
+//a.appendafter(div)
+//a.appendreplacewith(div)
