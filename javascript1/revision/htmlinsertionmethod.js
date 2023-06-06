5c2086c70774cbfb8ec067038b0f3a99829c1fc0
saurabhr570 <saurabhr570@gmail.com>
2023-06-06 11:28:34 +0530

    okay

---
+++ javascript1/revision/htmlinsertionmethod.js
@@ -0,0 +1,2 @@
+// let h1=document.getElementById("h1")
+// h1.innerHTML=h1.innerHTML+"<div>helllo this is new div tag</div>"
---
+++ javascript1/revision/one.js
@@ -0,0 +1,8 @@
+let a=document.getElementsByTagName("div")[0]
+let div=document.createElement("h1")
+div.innerHTML="hey i am new h1 tag(append)"
+// a.append(div)
+// a.prepend(div)
+// a.before(div)
+// a.after(div)
+// a.replaceWith(div)
